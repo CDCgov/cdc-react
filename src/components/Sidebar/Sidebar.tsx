25d8610c4f7152d4dd947d4acd22e6b20c3856ed
Kat Sublett <ksublett@fearless.tech>
2023-07-24 09:45:54 -0400

    create basic sidebar styling

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,21 +1,70 @@
+import { useState } from "react";
+import "./Sidebar.css";
+
+interface Item {
+  icon: string;
+  text: string;
+}
+interface Section {
+  heading: string;
+  items: Item[];
+}
 interface SidebarProps {
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+  isVisible: boolean;
+  sections: Section[];
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Sidebar = ({
-  ...props
-}: SidebarProps) => {
+export const Sidebar = ({}: SidebarProps) => {
+  const [isCollapsed, setCollapsed] = useState(false);
+
+  const toggleVisibility = () => {
+    setCollapsed(!isCollapsed);
+  };
+
   return (
-    <div
-      {...props}
-    >
-      Sidebar
+    <div className="sidebar">
+      <div className={`sidebar-full ${isCollapsed ? "sidebar-collapsed" : ""}`}>
+        <div className="menu_header">
+          <button
+            type="button"
+            className="usa-button usa-button--outline usa-button--inverse"
+            onClick={toggleVisibility}
+          >
+            {!isCollapsed ? "<<" : ">>"}
+          </button>
+        </div>
+
+        <div className="menu_item">
+          {!isCollapsed && <h2 className="heading">Insights</h2>}
+
+          <ul>
+            <li>
+              <span className="icon">🔥</span>
+              <span className="text">Dashboard</span>
+            </li>
+            <li>
+              <span className="icon">🔥</span>
+              <span className="text">Process Status</span>
+            </li>
+            <li>
+              <span className="icon">🔥</span>
+              <span className="text">Quality</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="menu_item">
+          {!isCollapsed && <h2 className="heading">Admin Tasks</h2>}
+        </div>
+        <div className="menu_footer">
+          <ul>
+            <li>
+              <span className="icon">❌</span>
+              <span className="text">Logout</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
