f0f0c4c2559d5c3508bd7a7cb6a26d10421d5383
Kat Sublett <ksublett@fearless.tech>
2023-07-25 15:47:45 -0400

    adjust icon alignment

--- src/components/Sidebar/Sidebar.test.tsx
+++ src/components/Sidebar/Sidebar.test.tsx
@@ -7,8 +7,8 @@ afterEach(() => {
 });
 
 describe("Sidebar", () => {
-  it("should render", () => {
-    render(<Sidebar />);
+  it("should render a button to collapse the sidebar", () => {
+    render(<Sidebar isVisible={false} sections={[]} />);
 
     expect(screen.getByText("Sidebar")).toBeInTheDocument();
   });
