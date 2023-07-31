a36b1cb85209ef8c0b5f1bcc20dfb66bf844fa35
cfarmer <cfarmer@fearless.tech>
2023-07-31 16:55:56 -0400

    boilerplate for button component and story

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "../components";
+
+const meta = {
+  title: "Components/Button",
+  component: Button,
+  argTypes: {},
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {},
+};
