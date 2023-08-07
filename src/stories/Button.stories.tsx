3ad54fd6b4a04f1951f37784155927661d2849f3
Ian Sawyer <49376379+ian-sawyer@users.noreply.github.com>
2023-08-07 04:49:20 -0400

    Added inverse style buttons

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -66,6 +66,87 @@ export const textButtonDisabled: Story = {
   },
 };
 
+export const unstyledButton: Story = {
+  args: {
+    variation: "unstyled",
+    children: (<span>Unstyled</span>)
+  },
+};
+
+export const unstyledButtonDisabled: Story = {
+  args: {
+    variation: "unstyled",
+    state: "disabled",
+    children: (<span>Unstyled</span>)
+  },
+};
+
+export const inverseButton: Story = {
+  args: {
+    inverse: true,
+    children: (<span>Inverse</span>)
+  },
+};
+
+export const inverseButtonDisabled: Story = {
+  args: {
+    inverse: true,
+    state: "disabled",
+    children: (<span>Disabled</span>)
+  },
+};
+
+export const inverseOutlineButton: Story = {
+  args: {
+    inverse: true,
+    variation: "outline",
+    children: (<span>Outline</span>)
+  },
+};
+
+export const inverseOutlineButtonDisabled: Story = {
+  args: {
+    inverse: true,
+    variation: "outline",
+    state: "disabled",
+    children: (<span>Outline</span>)
+  },
+};
+
+export const inverseTextButton: Story = {
+  args: {
+    inverse: true,
+    variation: "text",
+    children: (<span>Text</span>)
+  },
+};
+
+export const inverseTextButtonDisabled: Story = {
+  args: {
+    inverse: true,
+    variation: "text",
+    state: "disabled",
+    children: (<span>Text</span>)
+  },
+};
+
+export const inverseUnstyledButton: Story = {
+  args: {
+    inverse: true,
+    variation: "unstyled",
+    children: (<span>Unstyled</span>)
+  },
+};
+
+export const inverseUnstyledButtonDisabled: Story = {
+  args: {
+    inverse: true,
+    variation: "unstyled",
+    state: "disabled",
+    children: (<span>Unstyled</span>)
+  },
+};
+
 export const iconOnlyButton: Story = {
   args: {
     iconName: "user",
