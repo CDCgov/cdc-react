f48e512bbfdd8b67c9ac28bcb4eb0fd24d3c1efd
Ian Sawyer <49376379+ian-sawyer@users.noreply.github.com>
2023-08-10 13:35:01 -0400

    Added icon, DividerProps

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,12 +1,22 @@
-export const Divider = () => {
+interface DividerProps {
+  stroke: string;
+  height: number | string;
+  width?: number | string;
+  className?: string;
+}
+
+export const Divider = (props: DividerProps) => {
+  const { width, height, stroke, className } = props;
+
   return (
     <svg
-      width="auto"
-      height="2"
-      viewBox="0 0 auto 2"
+      className={className}
+      width={`${width || "auto"}`}
+      height={`${height}`}
+      viewBox={`0 0 ${width || "auto"} ${height}`}
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
-      <line x1="0" y1="0" x2="100%" y2="0" stroke="#BDBDBD" />
+      <line x1="0" y1="0" x2="100%" y2="0" stroke={stroke} />
     </svg>
   );
 };
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -190,6 +190,21 @@ const Icons = {
       />
     </svg>
   ),
+  squareArrowUpRight: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none">
+      <path
+        d="M5.52683 1L2 1C1.44772 1 1 1.44772 1 2V14C1 14.5523 1.44771 15 2 15L14 15C14.5523 15 15 14.5523 15 14V10.3333M6.25003 9.74999L15 1.00002M15 1.00002V6.83333M15 1.00002L9.16666 1.00001"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
   support: () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
