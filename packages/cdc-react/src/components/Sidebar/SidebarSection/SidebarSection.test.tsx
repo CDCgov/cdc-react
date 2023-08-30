87ae3be10a8991ca56fed477a9424102fdc3b8ad
Kat Sublett <ksublett@fearless.tech>
2023-08-30 13:37:17 -0400

    test: add MemoryRouter to test

--- packages/cdc-react/src/components/Sidebar/SidebarSection/SidebarSection.test.tsx
+++ packages/cdc-react/src/components/Sidebar/SidebarSection/SidebarSection.test.tsx
@@ -1,6 +1,7 @@
 import { cleanup, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { afterEach } from "vitest";
+import { MemoryRouter } from "react-router-dom";
 import { SidebarSection } from "./SidebarSection";
 
 afterEach(() => {
@@ -10,14 +11,16 @@ afterEach(() => {
 describe("Sidebar section component", () => {
   it("should work with keyboard allowing sequential tabbing", async () => {
     render(
-      <SidebarSection
-        heading="test"
-        items={[
-          { icon: "dashboard", text: "Dashboard" },
-          { icon: "process", text: "Process Status" },
-          { icon: "quality", text: "Quality" },
-        ]}
-      />
+      <MemoryRouter initialEntries={["/"]}>
+        <SidebarSection
+          heading="test"
+          items={[
+            { icon: "dashboard", text: "Dashboard", route: "dashboard" },
+            { icon: "process", text: "Process Status", route: "/" },
+            { icon: "quality", text: "Quality", route: "/" },
+          ]}
+        />
+      </MemoryRouter>
     );
 
     const anchorElements = screen.getAllByRole("link");
