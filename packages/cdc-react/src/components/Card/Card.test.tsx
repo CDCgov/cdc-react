ed99ffbbd7d4a530b9299c8a8b094b0cde67c38c
Kat Sublett <ksublett@fearless.tech>
2023-09-11 16:46:42 -0400

    test: add init component tests

--- packages/cdc-react/src/components/Card/Card.test.tsx
+++ packages/cdc-react/src/components/Card/Card.test.tsx
@@ -8,7 +8,38 @@ afterEach(() => {
 
 describe("Card component", () => {
   it("should render the card component", () => {
-    render(<Card layout="horizontal"><p>Test</p></Card>);
+    render(
+      <Card layout="horizontal">
+        <p>Test</p>
+      </Card>
+    );
     expect(screen.getByText("Test")).toBeInTheDocument();
+  });
+  it("should render in a horizontal layout", () => {
+    render(
+      <Card layout="horizontal">
+        <p>Horizontal Card</p>
+      </Card>
+    );
+    const card = screen.getByText("Horizontal Card").closest(".card-body");
+    expect(card).toHaveClass("horizontal");
+  });
+  it("should render in a vertical-left layout", () => {
+    render(
+      <Card layout="vertical-left">
+        <p>Vertical Left Card</p>
+      </Card>
+    );
+    const card = screen.getByText("Vertical Left Card").closest(".card-body");
+    expect(card).toHaveClass("vertical-left");
+  });
+  it("should render in a vertical-right layout", () => {
+    render(
+      <Card layout="vertical-right">
+        <p>Vertical Right Card</p>
+      </Card>
+    );
+    const card = screen.getByText("Vertical Right Card").closest(".card-body");
+    expect(card).toHaveClass("vertical-right");
   });
 });
