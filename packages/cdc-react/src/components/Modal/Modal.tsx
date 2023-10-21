cdae712801fba7775513a9f312812471643f2251
Kat Sublett <ksublett@fearless.tech>
2023-10-20 22:57:35 -0400

    feat: add story and expand modal structure

--- packages/cdc-react/src/components/Modal/Modal.tsx
+++ packages/cdc-react/src/components/Modal/Modal.tsx
@@ -1,46 +1,99 @@
 import "./Modal.scss";
 import { Button } from "@us-gov-cdc/cdc-react";
 import { Icons } from "@us-gov-cdc/cdc-react-icons";
+import { useEffect, useState } from "react";
 export interface ModalProps {
   isOpen: boolean;
-  onClose: () => void;
-  children: React.ReactNode;
-  modalTitle?: string;
-  modalBody?: string;
-  modalFooter?: string;
+  onClose?: () => void;
+  children?: React.ReactNode;
   isForcedAction?: boolean;
+}
+export interface ModalChildrenProps {
+  children: React.ReactNode;
 }
 
 export const Modal = (props: ModalProps) => {
+  useEffect(() => {
+    if (props.isOpen) {
+      document.body.style.overflow = "hidden";
+    }
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [props.isOpen]);
+
+  const [backgroundColor, setBackgroundColor] = useState("rgba(0,0,0,0.5)");
+
+  useEffect(() => {
+    if (!props.isOpen) {
+      setBackgroundColor("rgba(255,0,0,0.5)");
+    } else {
+      setBackgroundColor("rgba(0,0,0,0.5)");
+    }
+  }, [props.isOpen]);
+  if (!props.isOpen) return null;
+  useEffect(() => {
+    if (props.isOpen) {
+      document.body.style.overflow = "hidden";
+    }
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [props.isOpen]);
+
   return (
-    <div className="modal usa-modal">
-      <div className="usa-modal__content">
-        <div className="usa-modal__main">
-          <h2 className="usa-modal__heading">{props.modalTitle}</h2>
-          <div className="usa-prose">
-            <p>{props.modalBody}</p>
-          </div>
-          <div className="usa-modal__footer">
-            <ul className="usa-button-group">
-              <li className="usa-button-group__item">
-                <Button ariaLabel="continue">Continue without saving</Button>
-              </li>
-              <li className="usa-button-group__item">
-                <Button ariaLabel="go back" variation="text">
-                  Go back
-                </Button>
-              </li>
-            </ul>
-          </div>
+    <div
+      className="modal-overlay"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        background: "rgba(255, 105, 180, 0.8)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backdropFilter: "blur(5px)", // Blurred background
+        backgroundColor,
+      }}>
+      <div
+        className="modal usa-modal"
+        style={{
+          maxWidth: "600px",
+          margin: "100px auto",
+          background: "white",
+          padding: "20px",
+          borderRadius: "5px",
+        }}>
+        <div className="usa-modal__content">
+          <div className="usa-modal__main">{props.children}</div>
+          <Button
+            ariaLabel="Close this window"
+            size="tiny"
+            iconOnly={true}
+            icon={<Icons.Close />}
+            variation="unstyled"
+            className="usa-button usa-modal__close"
+            onClick={props.onClose}></Button>
         </div>
-        <Button
-          ariaLabel="Close this window"
-          size="tiny"
-          iconOnly={true}
-          icon={<Icons.Close />}
-          variation="unstyled"
-          className="usa-button usa-modal__close"></Button>
       </div>
     </div>
   );
+};
+
+export const ModalTitle = (props: ModalChildrenProps) => {
+  return <h2 className="modal-title usa-modal__heading">{props.children}</h2>;
+};
+
+export const ModalBody = (props: ModalChildrenProps) => {
+  return (
+    <div className="modal-body usa-prose">
+      <p>{props.children}</p>
+    </div>
+  );
+};
+
+export const ModalFooter = (props: ModalChildrenProps) => {
+  return <div className="modal-footer usa-modal__footer">{props.children}</div>;
 };
--- packages/storybook/src/stories/Modal.stories.tsx
+++ packages/storybook/src/stories/Modal.stories.tsx
@@ -1,5 +1,13 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { Modal } from "@us-gov-cdc/cdc-react";
+import { useArgs } from "@storybook/preview-api";
+// import { Modal } from "@us-gov-cdc/cdc-react";
+import {
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalTitle,
+} from "../../../cdc-react/src/components/Modal/Modal";
+import { Button } from "@us-gov-cdc/cdc-react";
 
 const meta: Meta<typeof Modal> = {
   title: "Components/Modal",
@@ -13,7 +21,39 @@ type Story = StoryObj<typeof Modal>;
 
 export const Example: Story = {
   args: {
-    modalTitle: "Are you sure you want to continue?",
-    modalBody: "You have unsaved changes that will be lost.",
+    isOpen: false,
+  },
+  render: function Render(args) {
+    const [{ isOpen }, updateArgs] = useArgs();
+
+    function handleOpenModal() {
+      updateArgs({ isOpen: !isOpen });
+    }
+
+    const handleCloseModal = () => {
+      updateArgs({ isOpen: !isOpen });
+      alert("Modal closed!");
+    };
+
+    return (
+      <>
+        <button onClick={handleOpenModal}>Open Modal</button>
+        <Modal {...args} isOpen={isOpen} onClose={handleCloseModal}>
+          <ModalTitle>Are you sure you want to continue?</ModalTitle>
+          <ModalBody>You have unsaved changes that will be lost.</ModalBody>
+          <ModalFooter>
+            <Button ariaLabel="continue" onClick={handleCloseModal}>
+              Continue without saving
+            </Button>
+            <Button
+              ariaLabel="go back"
+              variation="text"
+              onClick={handleCloseModal}>
+              Go back
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </>
+    );
   },
 };
