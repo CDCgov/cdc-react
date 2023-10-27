0c9668489f537d803e495d15e851ad9aa6130291
Kat Sublett <ksublett@fearless.tech>
2023-10-27 09:03:14 -0400

    test: init unit tests

--- packages/cdc-react/src/components/Modal/Modal.test.tsx
+++ packages/cdc-react/src/components/Modal/Modal.test.tsx
@@ -1,9 +1,42 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { Modal } from "./Modal";
+import { vi } from "vitest";
 
 describe("Modal component", () => {
-  it("should render the modal component", () => {
-    render(<Modal />);
-    expect(screen.getByText("Modal")).toBeInTheDocument();
+  it("should render the modal component with text inside", () => {
+    const { getByText } = render(
+      <Modal isOpen={true} onClose={() => {}}>
+        Modal
+      </Modal>
+    );
+
+    expect(getByText("Modal")).toBeVisible();
+  });
+
+  it("should not render the modal component when isOpen is false", () => {
+    const { queryByText } = render(
+      <Modal isOpen={false} onClose={() => {}}>
+        Modal
+      </Modal>
+    );
+    expect(queryByText("Modal")).toBeNull();
+  });
+
+  it("should call onClose when modal close button is clicked", () => {
+    const onCloseMock = vi.fn();
+
+    const { container } = render(
+      <Modal isOpen={true} onClose={onCloseMock}>
+        Modal
+      </Modal>
+    );
+    const closeButton = container.querySelector(".modal-close-btn");
+    if (!closeButton) {
+      throw new Error("button not found");
+    }
+
+    fireEvent.click(closeButton);
+
+    expect(onCloseMock).toHaveBeenCalledTimes(1);
   });
 });
