9af85da067574d44f3c6a696e77ca2495f1c05f8
cfarmer <cfarmer@fearless.tech>
2023-11-13 12:00:41 -0500

    slot and round corner shapes

--- packages/cdc-react/src/components/Pill/Pill.tsx
+++ packages/cdc-react/src/components/Pill/Pill.tsx
@@ -1,12 +1,24 @@
 import "./Pill.scss";
 
+export const pillShapes = ["slot", "roundCorners"];
+type PillShape = (typeof pillShapes)[number];
+
 export interface PillProps {
   text: string;
+  shape?: PillShape;
 }
 
-export const Pill = ({ text }: PillProps) => {
+export const Pill = ({ text, shape = "slot" }: PillProps) => {
+  let classes = "cdc-react";
+
+  if (shape == "roundCorners") {
+    classes += ` roundCorners`;
+  } else {
+    classes += ` slot`;
+  }
+
   return (
-    <span className="cdc-react">
+    <span className={classes}>
       <span className="content">{text}</span>
     </span>
   );
--- packages/storybook/src/stories/Pill.stories.tsx
+++ packages/storybook/src/stories/Pill.stories.tsx
@@ -1,10 +1,16 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Pill } from "@us-gov-cdc/cdc-react";
+import { Pill, pillShapes } from "@us-gov-cdc/cdc-react";
 
 const meta: Meta<typeof Pill> = {
   title: "Components/Pill",
   tags: ["autodocs"],
   component: Pill,
+  argTypes: {
+    shape: {
+      control: { type: "radio" },
+      options: [...pillShapes],
+    },
+  },
 };
 
 export default meta;
@@ -13,5 +19,6 @@ type Story = StoryObj<typeof Pill>;
 export const example: Story = {
   args: {
     text: "This is a pill",
+    shape: "slot",
   },
 };
