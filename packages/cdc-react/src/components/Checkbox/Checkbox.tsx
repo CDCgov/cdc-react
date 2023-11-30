8426ba285ffca9f9a10ee2d401505de958e56161
Kat Sublett <ksublett@fearless.tech>
2023-11-30 10:16:11 -0500

    feat: expand on checkbox component structure

--- packages/cdc-react/src/components/Checkbox/Checkbox.tsx
+++ packages/cdc-react/src/components/Checkbox/Checkbox.tsx
@@ -1,21 +1,46 @@
-import { ChangeEventHandler } from "react";
+// Checkbox.tsx
+import { ChangeEvent, ReactNode, useState } from "react";
+import "./Checkbox.scss";
 
 type CheckboxProps = {
-  ariaLabel: string;
-  checked: boolean;
+  id: string;
+  label?: string;
+  description?: string;
+  checkedBox?: boolean;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
   indeterminate?: boolean;
-  onChange?: ChangeEventHandler<HTMLInputElement>;
+  disabled?: boolean;
+  children?: ReactNode;
 };
 
 export const Checkbox = (props: CheckboxProps) => {
+  const [isChecked, setIsChecked] = useState(props.checkedBox);
+
+  const toggleCheckbox = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
-    <>
+    <label
+      htmlFor={props.id}
+      className={`checkbox-container 
+      ${isChecked ? "checked" : "unchecked"}
+      ${props.disabled ? "checkbox-disabled" : ""}
+      `}>
       <input
         type="checkbox"
-        aria-label={props.ariaLabel}
-        checked={props.checked}
-        onChange={props.onChange}
+        className="checkbox"
+        checked={isChecked}
+        onChange={toggleCheckbox}
+        id={props.id}
+        disabled={props.disabled}
       />
-    </>
+      <div className="checkbox-content">
+        <span className="label">{props.label}</span>
+        {props.description && (
+          <div className="label-description">{props.description}</div>
+        )}
+      </div>
+    </label>
   );
 };
