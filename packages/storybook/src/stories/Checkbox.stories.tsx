b7dc98df476d91b170dab308f6a923aa0d1d7c67
Kat Sublett <ksublett@fearless.tech>
2023-11-30 09:47:07 -0500

    feat(storybook): expand on checkbox story

--- packages/storybook/src/stories/Checkbox.stories.tsx
+++ packages/storybook/src/stories/Checkbox.stories.tsx
@@ -14,15 +14,51 @@ type Story = StoryObj<typeof Checkbox>;
 export const Example: Story = {
   args: {},
 };
-
-export const NestedCheckboxes: Story = {
+export const CheckboxSample: Story = {
   args: {},
-  render: function Render(args) {
+  render: function Render() {
     return (
       <>
-        <Checkbox {...args} />
-        <Checkbox {...args} />
-        <Checkbox {...args} />
+        <fieldset className="usa-fieldset">
+          <legend className="usa-legend">Select any historical figure</legend>
+
+          <Checkbox
+            id="person1"
+            label="Sojourner Truth"
+            description="This is optional text that can be used to describe the label in more detail."
+          />
+          <Checkbox
+            id="person2"
+            label="Frederick Douglass"
+            description="This is optional text that can be used to describe the label in more detail."
+            checkedBox={true}
+          />
+          <Checkbox
+            id="person3"
+            label="Booker T. Washington"
+            description="This is optional text that can be used to describe the label in more detail."
+          />
+          <Checkbox
+            id="person4"
+            label="George Washington Carver"
+            description="This is optional text that can be used to describe the label in more detail."
+            checkedBox={true}
+            disabled={true}
+          />
+        </fieldset>
+
+        <fieldset className="usa-fieldset">
+          <legend className="usa-legend">Select any historical figure</legend>
+
+          <Checkbox id="person1-nd" label="Sojourner Truth" checkedBox={true} />
+          <Checkbox id="person2-nd" label="Frederick Douglass" />
+          <Checkbox id="person3-nd" label="Booker T. Washington" />
+          <Checkbox
+            id="person4-nd"
+            label="George Washington Carver"
+            disabled={true}
+          />
+        </fieldset>
       </>
     );
   },
