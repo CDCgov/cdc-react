5a7b0080aa9bd05cebd7a693cfbd4ac6407b623a
Kat Sublett <ksublett@fearless.tech>
2023-12-07 11:30:20 -0500

    feat(storybook): add init stories

--- packages/storybook/src/stories/Checkbox.stories.tsx
+++ packages/storybook/src/stories/Checkbox.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { Checkbox } from "../../../cdc-react/src/components/Checkbox/Checkbox";
+import { Checkbox } from "@us-gov-cdc/cdc-react";
+import { ChangeEvent, useState } from "react";
 
 const meta: Meta<typeof Checkbox> = {
   title: "Components/Checkbox",
@@ -13,10 +14,57 @@ type Story = StoryObj<typeof Checkbox>;
 
 export const Example: Story = {
   args: {},
+  render: function Render() {
+    return (
+      <>
+        <Checkbox label="Single Checkbox" />
+      </>
+    );
+  },
 };
-export const CheckboxSample: Story = {
+
+export const IndeterminateCheckbox: Story = {
   args: {},
   render: function Render() {
+    const [checkedItems, setCheckedItems] = useState([false, false]);
+
+    const allChecked = checkedItems.every(Boolean);
+    const isIndeterminate = checkedItems.some(Boolean) && !allChecked;
+
+    return (
+      <>
+        <Checkbox
+          label="Parent Checkbox"
+          checkedBox={allChecked}
+          indeterminate={isIndeterminate}
+          onChange={(e) =>
+            setCheckedItems([e.target.checked, e.target.checked])
+          }></Checkbox>
+
+        <Checkbox
+          label="Child Checkbox 1"
+          checkedBox={checkedItems[0]}
+          onChange={(e) =>
+            setCheckedItems([e.target.checked, checkedItems[1]])
+          }></Checkbox>
+        <Checkbox
+          label="Child Checkbox 2"
+          checkedBox={checkedItems[1]}
+          onChange={(e) =>
+            setCheckedItems([checkedItems[0], e.target.checked])
+          }></Checkbox>
+      </>
+    );
+  },
+};
+export const CheckboxDescription: Story = {
+  args: {},
+  render: function Render() {
+    const [isCheckedBox, setCheckedBox] = useState(true);
+
+    const toggleCheckbox = (e: ChangeEvent<HTMLInputElement>) => {
+      setCheckedBox(e.target.checked);
+    };
     return (
       <>
         <fieldset className="usa-fieldset">
@@ -31,7 +79,8 @@ export const CheckboxSample: Story = {
             id="person2"
             label="Frederick Douglass"
             description="This is optional text that can be used to describe the label in more detail."
-            checkedBox={true}
+            checkedBox={isCheckedBox}
+            onChange={toggleCheckbox}
           />
           <Checkbox
             id="person3"
@@ -46,11 +95,29 @@ export const CheckboxSample: Story = {
             disabled={true}
           />
         </fieldset>
+      </>
+    );
+  },
+};
+export const CheckboxList: Story = {
+  args: {},
+  render: function Render() {
+    const [isCheckedBox, setCheckedBox] = useState(true);
 
+    const toggleCheckbox = (e: ChangeEvent<HTMLInputElement>) => {
+      setCheckedBox(e.target.checked);
+    };
+    return (
+      <>
         <fieldset className="usa-fieldset">
           <legend className="usa-legend">Select any historical figure</legend>
 
-          <Checkbox id="person1-nd" label="Sojourner Truth" checkedBox={true} />
+          <Checkbox
+            id="person1-nd"
+            label="Sojourner Truth"
+            checkedBox={isCheckedBox}
+            onChange={toggleCheckbox}
+          />
           <Checkbox id="person2-nd" label="Frederick Douglass" />
           <Checkbox id="person3-nd" label="Booker T. Washington" />
           <Checkbox
