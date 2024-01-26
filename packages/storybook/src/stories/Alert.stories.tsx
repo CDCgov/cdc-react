8124aa13d63c5184094da63254fa0f9c923ab8ab
Ty Bolt <tybolt@gmail.com>
2024-01-26 15:42:18 -0500

    small updates, wip how best to use or not use uswds in conjunction with custom styles

--- packages/storybook/src/stories/Alert.stories.tsx
+++ packages/storybook/src/stories/Alert.stories.tsx
@@ -6,50 +6,72 @@ const meta: Meta<typeof Alert> = {
   title: "Components/Alert",
   tags: ["autodocs"],
   component: Alert,
-  parameters: {
-    backgrounds: {
-      default: "default",
-      values: [
-        {
-          name: "default",
-          value: "#fff",
-        },
-        {
-          name: "inverse",
-          value: "#333333",
-        },
-      ],
-    },
-  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Alert>;
 
+export const Example: Story = {
+  argTypes: {
+    type: {
+      control: { type: "select" },
+      options: ["info", "warning", "success", "error", "emergency"],
+      description: `Alert types: "info", "warning", "success", "error", "emergency"`,
+      table: {
+        defaultValue: {
+          detail: "type is required",
+          summary: "null",
+        },
+      },
+    },
+    slim: {
+      control: { type: "boolean" },
+      description: `enable slim alert style/size`,
+    },
+    noIcon: {
+      control: { type: "boolean" },
+      description: `disable the display of the icon`,
+    },
+    heading: {
+      description: "main heading of the alert",
+    },
+    children: {
+      description:
+        "body content of the alert, can be a `string` or `React Node`",
+    },
+  },
+  args: {
+    type: "info",
+    heading: "Informational status",
+    slim: null,
+    noIcon: null,
+    children: (
+      <>
+        Lorem ipsum dolor sit amet,{" "}
+        <a href="#anchor">consectetur adipiscing elit</a>, sed do eiusmod.
+      </>
+    ),
+  },
+};
+
 export const basicAlerts: Story = {
   render: () => {
     return (
       <>
-        <h5>Info Alert:</h5>
-        <Alert type="info" heading="Informative Status">
-          <p>
-            THIS IS THE BODY with a <a href="#anchorsaway">link</a>
-          </p>
+        <h5>Standard alerts:</h5>
+        <Alert type="info" heading="Informative status">
+          Lorem ipsum dolor sit amet, <a href="#anchorsaway">link</a>
         </Alert>
-        <h5>Warning Alert:</h5>
-        <Alert type="warning" heading="Warning Status">
+        <Alert type="warning" heading="Warning status">
           This is the alert
         </Alert>
-        <h5>Success Alert:</h5>
-        <Alert type="success" heading="Success Status">
+        <Alert type="success" heading="Success status">
           This is the alert
         </Alert>
-        <h5>Error Alert:</h5>
-        <Alert type="error" heading="Error Status">
+        <Alert type="error" heading="Error status">
           This is the alert
         </Alert>
-        <h5>Emergency Alert:</h5>
-        <Alert type="emergency" heading="Emergency Status">
+        <Alert type="emergency" heading="Emergency status">
           This is the alert
         </Alert>
       </>
