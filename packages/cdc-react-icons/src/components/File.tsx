41ccb1299ffcd7fe55e93a0be7e7f968b2564e32
ZacharyRizer <zacharyrizer@gmail.com>
2024-03-29 15:01:32 -0400

    Added the file icon

---
+++ packages/cdc-react-icons/src/components/File.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from ".";
+
+export const File = ({ className }: IconProps) => {
+  return (
+    <svg
+      className={className}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M10 1H2.8C2.32261 1 1.86477 1.18964 1.52721 1.52721C1.18964 1.86477 1 2.32261 1 2.8V17.2C1 17.6774 1.18964 18.1352 1.52721 18.4728C1.86477 18.8104 2.32261 19 2.8 19H13.6C14.0774 19 14.5352 18.8104 14.8728 18.4728C15.2104 18.1352 15.4 17.6774 15.4 17.2V6.4M10 1L15.4 6.4M10 1V6.4H15.4M11.8 10.9H4.6M11.8 14.5H4.6M6.4 7.3H4.6"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
