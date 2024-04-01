487f11dc9661427a7ef57cd6af8669bda6e95ca2
ZacharyRizer <zacharyrizer@gmail.com>
2024-04-01 13:04:21 -0400

    Multiple stories setup

--- packages/storybook/src/stories/ProgressTracker.stories.tsx
+++ packages/storybook/src/stories/ProgressTracker.stories.tsx
@@ -1,20 +1,43 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { useArgs } from "@storybook/preview-api";
 import { ProgressTracker } from "@us-gov-cdc/cdc-react";
 
 const meta: Meta<typeof ProgressTracker> = {
   title: "Components/ProgressTracker",
   tags: ["autodocs"],
   component: ProgressTracker,
-  argTypes: {},
 };
 
 export default meta;
+
 type Story = StoryObj<typeof ProgressTracker>;
 
 export const Example: Story = {
   args: {
+    complete: false,
     currentAmount: 50,
+    indeterminate: false,
+    label: "Stage: Uploading",
     totalAmount: 100,
+  },
+};
+
+export const determinateNotComplete: Story = {
+  args: {
+    currentAmount: 50,
+    label: "Stage: Uploading",
+    totalAmount: 100,
+  },
+};
+
+export const indeterminate: Story = {
+  args: {
+    indeterminate: true,
+    label: "Stage: Validating",
+  },
+};
+
+export const complete: Story = {
+  args: {
+    complete: true,
   },
 };
